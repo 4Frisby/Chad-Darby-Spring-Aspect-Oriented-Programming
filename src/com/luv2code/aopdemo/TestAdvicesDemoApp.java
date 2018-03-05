fc6644e3ec62d0f159bbf44744786eab4d40aa0d
Tarık Nural <tariknural00@gmail.com>
2018-03-05 18:28:12 +0300

    commit.

--- src/com/luv2code/aopdemo/TestAdvicesDemoApp.java
+++ src/com/luv2code/aopdemo/TestAdvicesDemoApp.java
@@ -1,14 +1,17 @@
 package com.luv2code.aopdemo;
 
 import java.util.List;
+import java.util.logging.Logger;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import com.luv2code.aopdemo.dao.AccountDAO;
-import com.luv2code.aopdemo.dao.MembershipDAO;
 
-public class AfterReturningDemoApp {
+public class TestAdvicesDemoApp {
 
+	private static Logger myLogger = 
+					Logger.getLogger(TestAdvicesDemoApp.class.getName());
+			
 	public static void main(String[] args) {
 
 		// read spring config java class
@@ -18,16 +21,14 @@ public class AfterReturningDemoApp {
 		// get the bean from spring container
 		AccountDAO theAccountDAO = context.getBean("accountDAO", AccountDAO.class);
 		
-		// call method to find the accounts
-		List<Account> theAccounts = theAccountDAO.findAccounts(false);
+		myLogger.info("\nMain Program");
 		
-		// display the accounts
-		System.out.println("\n\nMain Program: AfterReturningDemoApp");
-		System.out.println("----");
+		myLogger.info("Calling findAccount");
 		
-		System.out.println(theAccounts);
-		
-		System.out.println("\n");
+		boolean tripWire = false;
+		List<Account> accounts = theAccountDAO.findAccounts(tripWire);
+				
+		myLogger.info("Finished");
 		
 		// close the context
 		context.close();
