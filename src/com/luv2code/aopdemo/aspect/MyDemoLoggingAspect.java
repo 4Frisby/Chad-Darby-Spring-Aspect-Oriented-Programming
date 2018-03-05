2b948ee04d46a95a015c11cec80b37f3f0fbd373
Tarık Nural <tariknural00@gmail.com>
2018-03-05 16:48:20 +0300

    commit.

--- src/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
+++ src/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -9,18 +9,23 @@ import org.springframework.stereotype.Component;
 @Component
 public class MyDemoLoggingAspect {
 
+	// pointcut declaration syntax.
 	@Pointcut("execution(* com.luv2code.aopdemo.dao.*.*(..))")
 	private void forDaoPackage() {}
 	
+	
+	// how to inject to the advice the created pointcut.
 	@Before("forDaoPackage()")
 	public void beforeAddAccountAdvice() {		
 		System.out.println("\n=====>>> Executing @Before advice on method");		
 	}
 	
+	// multiple advice declarations, if you do not spesificially mention the before order, spring will evaluate it by ascending character order.
 	@Before("forDaoPackage()")
 	public void performApiAnalytics() {
 		System.out.println("\n=====>>> Performing API analytics");		
 	}
+	
 	
 }
 
