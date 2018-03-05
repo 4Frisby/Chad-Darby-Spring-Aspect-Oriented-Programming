5387493d444272dd269db5f9c0b0be58f564a772
Tarık Nural <tariknural00@gmail.com>
2018-03-05 16:53:03 +0300

    commit.

--- src/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
+++ src/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -9,23 +9,30 @@ import org.springframework.stereotype.Component;
 @Component
 public class MyDemoLoggingAspect {
 
-	// pointcut declaration syntax.
 	@Pointcut("execution(* com.luv2code.aopdemo.dao.*.*(..))")
 	private void forDaoPackage() {}
 	
+	// create pointcut for getter methods
+	@Pointcut("execution(* com.luv2code.aopdemo.dao.*.get*(..))")
+	private void getter() {}
 	
-	// how to inject to the advice the created pointcut.
-	@Before("forDaoPackage()")
+	// create pointcut for setter methods
+	@Pointcut("execution(* com.luv2code.aopdemo.dao.*.set*(..))")
+	private void setter() {}
+	
+	// create pointcut: include package ... exclude getter/setter
+	@Pointcut("forDaoPackage() && !(getter() || setter())")
+	private void forDaoPackageNoGetterSetter() {}
+	
+	@Before("forDaoPackageNoGetterSetter()")
 	public void beforeAddAccountAdvice() {		
 		System.out.println("\n=====>>> Executing @Before advice on method");		
 	}
 	
-	// multiple advice declarations, if you do not spesificially mention the before order, spring will evaluate it by ascending character order.
-	@Before("forDaoPackage()")
+	@Before("forDaoPackageNoGetterSetter()")
 	public void performApiAnalytics() {
 		System.out.println("\n=====>>> Performing API analytics");		
 	}
-	
 	
 }
 
