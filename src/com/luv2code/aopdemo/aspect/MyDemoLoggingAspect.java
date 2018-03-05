a7257ca4276e3a316d3b6342e04f331d856797be
Tarık Nural <tariknural00@gmail.com>
2018-03-05 16:44:31 +0300

    commit.

--- src/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
+++ src/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -2,24 +2,26 @@ package com.luv2code.aopdemo.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class MyDemoLoggingAspect {
 
-	// this is where we add all of our related advices for logging
+	@Pointcut("execution(* com.luv2code.aopdemo.dao.*.*(..))")
+	private void forDaoPackage() {}
 	
-	// let's start with an @Before advice
-
-	//execution (* -- > any return type, com.luv2code.aopdemo.dao -- > in this package, com.luv2code.aopdemo.dao.* -- > in this package any class,
-	// com.luv2code.aopdemo.dao.*.* -- > in this package any classes any method, ".." -- > any number of arguments.
-	@Before("execution(* com.luv2code.aopdemo.dao.*.*(..))")
-	public void beforeAddAccountAdvice() {
-		
-		System.out.println("\n=====>>> Executing @Before advice on method");
-		
+	@Before("forDaoPackage()")
+	public void beforeAddAccountAdvice() {		
+		System.out.println("\n=====>>> Executing @Before advice on method");		
 	}
+	
+	@Before("forDaoPackage()")
+	public void performApiAnalytics() {
+		System.out.println("\n=====>>> Performing API analytics");		
+	}
+	
 }
 
 
